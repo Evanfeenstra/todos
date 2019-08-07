717298906166df1a6567d5876ef442f2ebd6ee59
Evan Feenstra <evan@evanfeenstra.com>
2019-08-07 13:02:41 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Checkbox} from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    todos:[{
+      text:'Get Married',
+      completed:true, 
+      dueDate:''
+    }]
+  }
+
+  render(){
+    const {todos} = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          Todo List
+        </header>
+        <Button>hi</Button>
+        {todos.map((t,i)=>{
+          return <Item key={i} {...t} 
+            setCompleted={(e)=>console.log(e)}
+          />
+        })}
+      </div>
+    );
+  }
+}
+
+function Item({text, completed, dueDate, setCompleted, setDueDate}) {
+  return (<div className="item">
+    <Checkbox checked={completed} onChange={setCompleted}/>
+    <span className="item-text">{text}</span>
+    <span className="item-duedate">{dueDate}</span>
+  </div>)
 }
 
 export default App;
